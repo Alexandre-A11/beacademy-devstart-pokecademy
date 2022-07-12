3af79fd1289919d54885001c7b932e92fbda88f5
Alexandre Almeida <alee.wxa@gmail.com>
2022-07-11 23:13:23 -0300

    First Commit :)

---
+++ projeto-individual/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
